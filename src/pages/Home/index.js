4c7547947ae2278d24076bf67185e8d3e2bf82e5
Jean <rjeangonzalo@gmail.com>
2019-11-11 05:23:40 -0500

    add style in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,12 @@
 import React, { useState } from 'react';
 
 import { Container, Typography, Card, Grid, TextField, Button } from '@material-ui/core';
+import styles from './style';
 
 export default () => {
 
     const [searchText, setSearchText] = useState('');
-
+    const classes = styles();
     const handleSearchTextChange = event => {
         setSearchText(event.target.value);
     };
@@ -14,11 +15,11 @@ export default () => {
     const handleCleanTextCLick = event => { console.log('this clean') };
     const handleSearchTextClick = event => { console.log('this search') };
     return (
-        <Container>
-            <Card>
-                <Grid container>
+        <Container className={classes.container}>
+            <Card className={classes.cardContainer}>
+                <Grid container className={classes.titleGridContainer}>
                     <Grid>
-                        <Typography>Bienvenido prro</Typography>
+                        <Typography className={classes.title}>Bienvenido!</Typography>
                     </Grid>
                     <Grid>
                         <label>Icono</label>
@@ -27,10 +28,11 @@ export default () => {
                 <TextField
                     value={searchText}
                     placeholder="Buscar ..."
+                    className={classes.textFieldSearch}
                     onChange={handleSearchTextChange} />
-                <Grid>
+                <Grid className={classes.buttonsContainer}>
                     <Button variant="contained" onClick={handleCleanTextCLick}>Limpiar</Button>
-                    <Button variant="contained" color="primary" onClick={handleSearchTextClick}>Buscar</Button>
+                    <Button variant="contained" className={classes.searchButton} color="primary" onClick={handleSearchTextClick}>Buscar</Button>
                 </Grid>
             </Card>
         </Container>)

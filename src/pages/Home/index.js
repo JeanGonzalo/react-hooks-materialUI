cf3184436257c894f11536fb1f5cfa8492e8ab7a
Jean <rjeangonzalo@gmail.com>
2019-11-11 04:21:20 -0500

    Creando Home Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,39 @@
 //*Functional component*
-import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+
+import { Container, Typography, Card, Grid, TextField, Button } from '@material-ui/core';
 
 export default () => {
-    return (<Container><Typography>Home</Typography></Container>)
+
+    const [searchText, setSearchText] = useState('');
+
+    const handleSearchTextChange = event => {
+        setSearchText(event.target.value);
+    };
+
+    const handleCleanTextCLick = event => { console.log('this clean') };
+    const handleSearchTextClick = event => { console.log('this search') };
+    return (
+        <Container>
+            <Card>
+                <Grid container>
+                    <Grid>
+                        <Typography>Bienvenido prro</Typography>
+                    </Grid>
+                    <Grid>
+                        <label>Icono</label>
+                    </Grid>
+                </Grid>
+                <TextField
+                    value={searchText}
+                    placeholder="Buscar ..."
+                    onChange={handleSearchTextChange} />
+                <Grid>
+                    <Button variant="contained" onClick={handleCleanTextCLick}>Limpiar</Button>
+                    <Button variant="contained" color="primary" onClick={handleSearchTextClick}>Buscar</Button>
+                </Grid>
+            </Card>
+        </Container>)
 }
 
 
